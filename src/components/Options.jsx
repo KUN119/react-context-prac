3ff9f5a1db5a80acbc1d83613031642c21624524
KUN119 <kjp2629@naver.com>
2024-06-09 13:38:27 +0900

    options 컴포넌트 구현

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,20 +1,13 @@
 import React, { useState } from 'react'
 
-export default function Options({ name }) {
-    const [checked, setChecked] = useState(false);
-
-    const handleChangeCheckbox = (e) => {
-        const { checked } = e.target
-        setChecked(checked)
-    }
-
+export default function Options({ name, updateItemCount }) {
     return (
         <>
             <section>
                 <label className='flex mb-2'>
                     <input 
                         type="checkbox" 
-                        onChange={handleChangeCheckbox}
+                        onChange={(e) => updateItemCount(name, e.target.checked ? 1 : 0)}
                     />
                     <span>&nbsp;{name}</span>
                 </label>
