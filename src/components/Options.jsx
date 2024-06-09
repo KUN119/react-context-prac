28e7697b0ad47dd361ea5aa6be4c8c14ee07250c
KUN119 <kjp2629@naver.com>
2024-06-09 11:40:57 +0900

    feature: options 화면 구현

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Options() {
-  return (
-    <div>Options</div>
-  )
+export default function Options({ name }) {
+    const [checked, setChecked] = useState(false);
+
+    const handleChangeCheckbox = (e) => {
+        const { checked } = e.target
+        setChecked(checked)
+    }
+
+    return (
+        <>
+            <section>
+                <label className='flex mb-2'>
+                    <input 
+                        type="checkbox" 
+                        onChange={handleChangeCheckbox}
+                    />
+                    <span>&nbsp;{name}</span>
+                </label>
+            </section>
+        </>
+    )
 }
